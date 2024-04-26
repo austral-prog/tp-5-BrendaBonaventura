54fd1286875a13b3efb7f462cf4d1336a888304a
BrendaBonaventura <162723248+BrendaBonaventura@users.noreply.github.com>
2024-04-26 12:15:04 -0300

    Update maximums.py

--- maximums.py
+++ maximums.py
@@ -1,17 +1,15 @@
-def max_of_two(x,y):
-    if (x>y):
-        return x 
-    else: (y>x)
-    return y
-maxtwo= max_of_two(5,1)
-print(maxtwo)
+def max_of_two(x, y):
+    if x > y:
+        return x
+    else:
+        return y
+
+
 
 def max_of_three(x, y, z):
-    if x>z and x>y:
-        return x
-    elif x<y and y>z:
+    if x <= y and z <= y:
         return y
-    else: x<z and y<z
-    return z
-maxthree= max_of_three(100,70,4)
-print(maxthree)
+    elif y <= x and z <= x:
+        return x
+    elif y <= z and x <= z:
+        return z
