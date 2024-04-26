5d95b6fcb072e5e140b081a9409cda1837dd3c53
BrendaBonaventura <162723248+BrendaBonaventura@users.noreply.github.com>
2024-04-26 12:16:32 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,51 +1,42 @@
 def roots(a, b, c):
-    import math
-    discriminant= b**2 - 4*a*c
-    if discriminant>0:
-        r1 = (-b + math.sqrt(discriminant)) / 2*a
-        r2 = (-b - math.sqrt(discriminant)) / 2*a
-        return f"({r1}, {r2})"
-    elif discriminant == 0:
-        r = -b / (2*a)
-        return f"({r})"
-    else:
-        return "( )"
+    discriminant = b**2 - 4*a*c
 
-raiz= roots(1,-3,2)
-print(raiz)
-    
+    if discriminant == 0:
+        root_0 = (-b + discriminant)/(2*a)
+        return f'({root_0})'
+    elif discriminant > 0:
+        root_11 = (-b + math.sqrt(discriminant))/(2*a)
+        root_12 = (-b - math.sqrt(discriminant))/(2*a)
+        return f'({root_11}, {root_12})'
+    else:
+        return '( )'
+
+
+
 def value_y(a, b, c, x):
-    y= a*(x**2) + b*x + c
+    y = a*x**2 + b*x + c
     return y
-cuad= value_y(1,-3,2,0)
-print(cuad)
+
+
 
 def to_string(a, b, c):
-    if a!=0 and b!=0 and c!=0:
-        return f"f(x) = {a} * X^2 + {b} * X + {c}"
-    elif a==0 and b!=0 and c!=0:
+    if a == 0 and not b ==0 and not c ==0:
         return f"f(x) = {b} * X + {c}"
-    elif a!=0 and b==0 and c!=0:
+    elif b == 0 and not c == 0 and not a == 0:
         return f"f(x) = {a} * X^2 + {c}"
-    elif a!=0 and b!=0 and c==0:
+    elif c == 0 and not b == 0 and not a == 0:
         return f"f(x) = {a} * X^2 + {b} * X"
-    elif a==0 and b==0 and c!=0:
+    elif a == 0 and b == 0 and not c == 0:
         return f"f(x) = {c}"
-    elif a==0 and b!=0 and c==0:
-        return f"f(x) = {b} * X"
-    elif a!=0 and b==0 and c==0:
-        return f"f(x) = {a} * X^2"
-string= to_string(2,-3,1)
-print(string)
+    else:
+        return f"f(x) = {a} * X^2 + {b} * X + {c}"
 
-def derivation(a, b, c):
-    if a!=0 and b!=0:
-        return f"f'(x) = {2*a} * X + {b}"
-    elif a==0 and b!=0:
+
+
+def derivation(a, b):
+    if a == 0:
         return f"f'(x) = {b}"
-    elif a!=0 and b==0:
+    elif b == 0:
         return f"f'(x) = {2*a} * X"
-    elif a==0 and b==0:
-        return f"f'(x) = 0"   
-deriv=derivation(2,-3,1)
-print(deriv)
+    else:
+        return f"f'(x) = {a*2} * X + {b}"
